6b558d1e7473672163a7ffcaf9329e0fb82d5a2a
maritoo17 <msancgim@myuax.com>
2024-04-18 17:45:36 +0200

    1

---
+++ src/main/java/DatosDinamicos/Pareja.java
@@ -0,0 +1,32 @@
+package DatosDinamicos;
+
+public class Pareja <T, U> {
+    private T primerElemento;
+    private U segundoElemento;
+
+    public Pareja(T primerElemento, U segundoElemento) {
+        this.primerElemento = primerElemento;
+        this.segundoElemento = segundoElemento;
+    }
+
+    public T getPrimerElemento() {
+        return primerElemento;
+    }
+
+    public void setPrimerElemento(T primerElemento) {
+        this.primerElemento = primerElemento;
+    }
+
+    public U getSegundoElemento() {
+        return segundoElemento;
+    }
+
+    public void setSegundoElemento(U segundoElemento) {
+        this.segundoElemento = segundoElemento;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + primerElemento + ", " + segundoElemento + ")";
+    }
+}

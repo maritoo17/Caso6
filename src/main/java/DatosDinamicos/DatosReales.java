58e1f8497602576c62fb8e966eb4fd4bb92061e4
maritoo17 <msancgim@myuax.com>
2024-04-21 11:14:10 +0200

    17

--- src/main/java/DatosDinamicos/DatosReales.java
+++ src/main/java/DatosDinamicos/DatosReales.java
@@ -1,5 +1,6 @@
 package DatosDinamicos;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,5 +17,14 @@ public class DatosReales {
 
     public List<Double> obtenerLista() {
         return lista;
+    }
+
+    public void mostrarEnVentana() {
+        JFrame frame = new JFrame("DatosReales");
+        JLabel label = new JLabel(this.lista.toString());
+        frame.getContentPane().add(label);
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }

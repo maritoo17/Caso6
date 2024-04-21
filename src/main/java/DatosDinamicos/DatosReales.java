ce5b71f37acab1cc9537798093ceb6e1e34382a7
maritoo17 <msancgim@myuax.com>
2024-04-21 11:39:34 +0200

    26

--- src/main/java/DatosDinamicos/DatosReales.java
+++ src/main/java/DatosDinamicos/DatosReales.java
@@ -1,6 +1,9 @@
 package DatosDinamicos;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,8 +24,19 @@ public class DatosReales {
 
     public void mostrarEnVentana() {
         JFrame frame = new JFrame("DatosReales");
+        frame.setLayout(new BorderLayout());
         JLabel label = new JLabel(this.lista.toString());
-        frame.getContentPane().add(label);
+        frame.getContentPane().add(label, BorderLayout.CENTER);
+
+        JButton backButton = new JButton("Atrás");
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+            }
+        });
+        frame.getContentPane().add(backButton, BorderLayout.SOUTH);
+
         frame.setSize(300, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);

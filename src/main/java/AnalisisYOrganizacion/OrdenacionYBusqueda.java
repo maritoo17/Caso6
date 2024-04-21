496f4ca391255ed0ccbc3853cc3c101cdb035734
maritoo17 <msancgim@myuax.com>
2024-04-21 11:39:58 +0200

    27

--- src/main/java/AnalisisYOrganizacion/OrdenacionYBusqueda.java
+++ src/main/java/AnalisisYOrganizacion/OrdenacionYBusqueda.java
@@ -1,6 +1,9 @@
 package AnalisisYOrganizacion;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -43,8 +46,19 @@ public class OrdenacionYBusqueda {
 
     public void mostrarEnVentana() {
         JFrame frame = new JFrame("OrdenacionYBusqueda");
+        frame.setLayout(new BorderLayout());
         JLabel label = new JLabel(this.ventas.toString());
-        frame.getContentPane().add(label);
+        frame.getContentPane().add(label, BorderLayout.CENTER);
+
+        JButton backButton = new JButton("Atrás");
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+            }
+        });
+        frame.getContentPane().add(backButton, BorderLayout.SOUTH);
+
         frame.setSize(300, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);

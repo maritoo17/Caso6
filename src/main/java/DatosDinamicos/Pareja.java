dc6789449e72eb5d40a5a82e7b67a80120e9138c
maritoo17 <msancgim@myuax.com>
2024-04-21 11:38:58 +0200

    24

--- src/main/java/DatosDinamicos/Pareja.java
+++ src/main/java/DatosDinamicos/Pareja.java
@@ -1,6 +1,9 @@
 package DatosDinamicos;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Pareja <T, U> {
     private T primerElemento;
@@ -34,8 +37,19 @@ public class Pareja <T, U> {
 
     public void mostrarEnVentana() {
         JFrame frame = new JFrame("Pareja");
+        frame.setLayout(new BorderLayout());
         JLabel label = new JLabel(this.toString());
-        frame.getContentPane().add(label);
+        frame.getContentPane().add(label, BorderLayout.CENTER);
+
+        JButton backButton = new JButton("Atrás");
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+            }
+        });
+        frame.getContentPane().add(backButton, BorderLayout.SOUTH);
+
         frame.setSize(300, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);

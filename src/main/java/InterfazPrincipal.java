8026d4f54e4c6a4ff8db27cd475bf08a30c0d548
maritoo17 <msancgim@myuax.com>
2024-04-21 11:34:59 +0200

    23

--- src/main/java/InterfazPrincipal.java
+++ src/main/java/InterfazPrincipal.java
@@ -1,4 +1,7 @@
 import DatosDinamicos.*;
+import AnalisisYOrganizacion.*;
+import MapaYAsociacion.*;
+import IndexacionYVisualizacion.*;
 
 import javax.swing.*;
 import java.awt.*;
@@ -28,11 +31,63 @@ public class InterfazPrincipal extends JFrame {
             }
         });
 
-        // Agrega botones similares para las otras opciones...
+        JButton boton3 = new JButton("Ejecutar DatosReales");
+        boton3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DatosReales datosReales = new DatosReales();
+                datosReales.agregarDato(1.23);
+                datosReales.mostrarEnVentana();
+            }
+        });
+
+        JButton boton4 = new JButton("Ejecutar InterfazUsuario");
+        boton4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                InterfazUsuario interfazUsuario = new InterfazUsuario();
+                interfazUsuario.setVisible(true);
+            }
+        });
+
+        JButton boton5 = new JButton("Ejecutar OrdenacionYBusqueda");
+        boton5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                OrdenacionYBusqueda ordenacionYBusqueda = new OrdenacionYBusqueda();
+                ordenacionYBusqueda.agregarVenta(new Venta("Carlos", 200.0, "Electrónicos"));
+                ordenacionYBusqueda.ordenarVentasPorMonto();
+                ordenacionYBusqueda.mostrarEnVentana();
+            }
+        });
+
+        JButton boton6 = new JButton("Ejecutar GestionDeRelaciones");
+        boton6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                GestionDeRelaciones gestionDeRelaciones = new GestionDeRelaciones();
+                gestionDeRelaciones.agregarRelacionNumerosYLetras(1, "Uno");
+                gestionDeRelaciones.mostrarEnVentana();
+            }
+        });
+
+        JButton boton7 = new JButton("Ejecutar IndexacionRecursiva");
+        boton7.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                IndexacionRecursiva indexacionRecursiva = new IndexacionRecursiva();
+                indexacionRecursiva.indexarDirectorio("/path/to/directory");
+                indexacionRecursiva.mostrarEnVentana();
+            }
+        });
 
         add(boton1);
         add(boton2);
-        // Agrega los otros botones al panel...
+        add(boton3);
+        add(boton4);
+        add(boton5);
+        add(boton6);
+        add(boton7);
 
         setSize(300, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);

dd663c26202362971b63bdffca0ea862ca64c586
maritoo17 <msancgim@myuax.com>
2024-04-23 17:51:24 +0200

    39

--- src/main/java/AnalisisYOrganizacion/Venta.java
+++ src/main/java/AnalisisYOrganizacion/Venta.java
@@ -1,14 +1,58 @@
 package AnalisisYOrganizacion;
 
-public class Venta implements Comparable<Venta> {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Venta extends JFrame {
     private String nombre;
     private double monto;
     private String categoria;
+    private JTextField nombreField;
+    private JTextField montoField;
+    private JTextField categoriaField;
+    private JTextArea textArea;
+    private List<Venta> ventas;
 
-    public Venta(String nombre, double monto, String categoria) {
-        this.nombre = nombre;
-        this.monto = monto;
-        this.categoria = categoria;
+    public Venta() {
+        ventas = new ArrayList<>();
+        createUI();
+    }
+
+    private void createUI() {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(500, 400);
+        setLayout(new FlowLayout());
+
+        nombreField = new JTextField(10);
+        montoField = new JTextField(10);
+        categoriaField = new JTextField(10);
+        textArea = new JTextArea(10, 30);
+        JButton addButton = new JButton("Agregar Venta");
+
+        add(new JLabel("Nombre:"));
+        add(nombreField);
+        add(new JLabel("Monto:"));
+        add(montoField);
+        add(new JLabel("Categoría:"));
+        add(categoriaField);
+        add(addButton);
+        add(new JScrollPane(textArea));
+
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                nombre = nombreField.getText();
+                monto = Double.parseDouble(montoField.getText());
+                categoria = categoriaField.getText();
+                Venta venta = new Venta();
+                ventas.add(venta);
+                updateTextArea();
+            }
+        });
     }
 
     public String getNombre() {
@@ -23,8 +67,10 @@ public class Venta implements Comparable<Venta> {
         return categoria;
     }
 
-    @Override
-    public int compareTo(Venta o) {
-        return Double.compare(this.monto, o.monto);
+    private void updateTextArea() {
+        textArea.setText("");
+        for (Venta venta : ventas) {
+            textArea.append(venta.getNombre() + ", " + venta.getMonto() + ", " + venta.getCategoria() + "\n");
+        }
     }
 }

6ef88fa37b1bcdc82dfaed801818f109c2879664
maritoo17 <msancgim@myuax.com>
2024-04-23 19:46:23 +0200

    69

--- src/main/java/IndexacionYVisualizacion/IndexacionRecursiva.java
+++ src/main/java/IndexacionYVisualizacion/IndexacionRecursiva.java
@@ -1,71 +1,29 @@
 package IndexacionYVisualizacion;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
-public class IndexacionRecursiva extends JFrame {
-    private Map<String, String> indice;
-    private JTextArea textArea;
+public class IndexacionRecursiva {
+    private final Map<String, String> index = new HashMap<>();
 
-    public IndexacionRecursiva() {
-        this.indice = new HashMap<>();
-        createUI();
-    }
-
-    private void createUI() {
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(500, 400);
-        setLayout(new FlowLayout());
-
-        textArea = new JTextArea(10, 30);
-        JButton indexButton = new JButton("Iniciar Indexación");
-
-        add(indexButton);
-        add(new JScrollPane(textArea));
-
-        indexButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                indexarDirectorio("/path/to/directory");
-                listarArchivosOrdenados();
-            }
-        });
-    }
-
-    public void indexarDirectorio(String rutaDirectorio) {
-        File directorio = new File(rutaDirectorio);
+    public void indexar(File directorio) {
         File[] archivos = directorio.listFiles();
-
         if (archivos != null) {
             for (File archivo : archivos) {
                 if (archivo.isDirectory()) {
-                    indexarDirectorio(archivo.getPath());
+                    indexar(archivo);
                 } else {
-                    indice.put(archivo.getName(), archivo.getPath());
+                    index.put(archivo.getName(), archivo.getAbsolutePath());
                 }
             }
         }
     }
 
-    public void listarArchivosOrdenados() {
-        Map<String, String> indiceOrdenado = new TreeMap<>(indice);
-        for (Map.Entry<String, String> entrada : indiceOrdenado.entrySet()) {
-            textArea.append("Nombre del archivo: " + entrada.getKey() + ", Ruta completa: " + entrada.getValue() + "\n");
-        }
+    public String buscar(String nombreArchivo) {
+        return index.get(nombreArchivo);
     }
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new IndexacionRecursiva().setVisible(true);
-            }
-        });
+    public Map<String, String> getIndex() {
+        return index;
     }
 }

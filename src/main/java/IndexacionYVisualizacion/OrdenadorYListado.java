acb18ad0d831277d1b8908418977873d01ee9bc6
maritoo17 <msancgim@myuax.com>
2024-04-23 19:46:34 +0200

    70

---
+++ src/main/java/IndexacionYVisualizacion/OrdenadorYListado.java
@@ -0,0 +1,19 @@
+package IndexacionYVisualizacion;
+
+import java.util.*;
+
+public class OrdenadorYListado {
+    private final Map<String, String> archivos = new TreeMap<>();
+
+    public void agregar(String nombreArchivo, String rutaArchivo) {
+        archivos.put(nombreArchivo, rutaArchivo);
+    }
+
+    public List<String> listar() {
+        List<String> lista = new ArrayList<>();
+        for (Map.Entry<String, String> entrada : archivos.entrySet()) {
+            lista.add(entrada.getKey() + " - " + entrada.getValue());
+        }
+        return lista;
+    }
+}
